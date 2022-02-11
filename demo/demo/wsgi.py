d89379c5e5fbc05d07745fe2a071653679989ef0
Priya <92863151+priya1puresoftware@users.noreply.github.com>
2022-02-11 12:59:12 +0530

    Update wsgi.py

--- demo/demo/wsgi.py
+++ demo/demo/wsgi.py
@@ -6,9 +6,9 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
+
 import os
-import sys
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.append(BASE_DIR)
-os.environ['DJANGO_SETTINGS_MODULE'] = 'demo.settings'
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "demo.settings")
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
