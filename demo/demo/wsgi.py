b20cf674c9e91d667271d78f1c69ddf41be7d2cf
Priya <92863151+priya1puresoftware@users.noreply.github.com>
2022-02-11 11:56:16 +0530

    Update wsgi.py

--- demo/demo/wsgi.py
+++ demo/demo/wsgi.py
@@ -7,8 +7,9 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
-import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "demo.settings")
-
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+    import os
+    import sys
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    sys.path.append(BASE_DIR)
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'YOURAPP.settings'
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "YOURAPP.settings")
